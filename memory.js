d67a11c1721f0ecc49c5b948678a9e36aa795b37
Kido Atube <kidoatube@gmail.com>
2019-05-02 14:15:36 +0100

    randomly place pictures

--- memory.js
+++ memory.js
@@ -1,9 +1,11 @@
 console.log("Getting there!");
 
 let panels = document.querySelectorAll(".container div");
+console.dir(panels)
 
 let arr = [];
-
+let classArray = ["fa-angular", "fa-node-js", "fa-vuejs", "fa-grunt", "fa-js", "fa-react", "fa-github", "fa-css3", "fa-html5", "fa-python"]
+let classNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 let genRand = function () {
   let i;
   for (i = 0; i < 20; i++) {
@@ -14,15 +16,26 @@ let genRand = function () {
         arr.push(rand);
       }
     }
+    let k = 0;
+    arr.forEach(num => {
+      if (k >= 10) {
+        k = 0;
+      };
+      panels[num].classList.add("rotate", `${classArray[k]}`);
+      k++
+    })
   }
+
   console.log(arr);
 }
 
 let add = function () {
-  this.classList.add("rotate", "fab", "fa-angular")
+  this.classList.add("rotate")
 }
 
 
 panels.forEach(panel => {
   panel.addEventListener("click", genRand)
 })
+
+// window.addEventListener("load", genRand);
