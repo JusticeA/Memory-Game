a90c9be041a409669d5cdad36c214c2ae9128d4a
Kido Atube <kidoatube@gmail.com>
2019-05-07 16:45:55 +0100

    Add restart functionality and also number of tries to fully match every panel

--- memory.js
+++ memory.js
@@ -1,10 +1,14 @@
 /* Variables */
 
 let panels = document.querySelectorAll(".container div");
+console.log([...panels])
 let audio = document.querySelectorAll("audio");
 let classArray = ["fa-angular", "fa-node-js", "fa-vuejs", "fa-grunt", "fa-js", "fa-react", "fa-github", "fa-css3", "fa-html5", "fa-python"];
 let classNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arr = [];
+const clicks = document.querySelector("p .clicks");
+const bestScore = document.querySelector("p .bscore");
+
 
 /* Functionality */
 
@@ -32,25 +36,65 @@ let genRand = function () {
 
 let lastClicked;
 let add = function () {
+  clicks.textContent++
+  if (clicks.textContent >= 38) {
+    clicks.style.color = "#7b1043c7"
+    clicks.style.animationName = "animate"
+  }
+  let newClicked;
+  audio[0].currentTime = 0;
   audio[0].play();
   panels.forEach(panel => {
-    let newClicked;
     if (panel.classList.contains("rotate")) {
       newClicked = this.classList;
       console.log(newClicked);
       if (newClicked.item(1) === lastClicked.item(1) && !newClicked.contains("rotate")) {
         audio[2].play();
-        newClicked.value = "visible";
-        lastClicked.value = "visible";
+        newClicked.value = "invisible";
+        lastClicked.value = "invisible";
+       
       } else {
-        // newClicked.remove("rotate");
         lastClicked.remove("rotate");
+        newClicked.remove("rotate");
       }
     }
   });
   this.classList.add("rotate");
   lastClicked = this.classList;
   console.log(lastClicked.item(1));
+  restart();
+}
+
+let lastCount;
+const restart = function () {
+  let checker = Array.from(panels).every(panel => {
+    return (panel.classList.contains("invisible"));
+  });
+  let delay = () => {
+    panels.forEach(panel => {
+      panel.classList.remove("rotate", "invisible")
+      panel.classList.add("fab")
+    })
+    arr = []
+    genRand();
+    // window.location.reload()
+  }
+  if (checker) {
+    clicks.textContent = 0;
+    clicks.style.color = "#AAB8E5"
+    clicks.style.animationName = "notanimate"
+    let newCount = clicks.textContent;
+    console.log(newCount)
+    if (newCount <= lastCount) {
+      console.log("%c Okay!", "font-size: 34px; color: red;")
+      // lastCount.style.color = "#7b1043c7"
+      bestScore.style.color = "#7b1043c7"
+      bestScore.textContent = ` / ${lastCount} `
+    }
+    setTimeout(delay, 1000);
+  }
+  lastCount = clicks.textContent;
+  console.log(lastCount);
 }
 
 /* Event Listeners */
@@ -59,5 +103,5 @@ panels.forEach(panel => {
   panel.addEventListener("click", add);
 })
 
-
 window.addEventListener("load", genRand);
+
